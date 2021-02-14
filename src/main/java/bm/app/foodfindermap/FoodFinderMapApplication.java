846fc28830190d90284417b659b1a5cf42c7f2cb
Dazikiri Anikar <mierzwinski.b@gmail.com>
2021-02-14 12:54:56 +0100

    The initial commit.

---
+++ src/main/java/bm/app/foodfindermap/FoodFinderMapApplication.java
@@ -0,0 +1,13 @@
+package bm.app.foodfindermap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodFinderMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodFinderMapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bm/app/foodfindermap/FoodFinderMapApplicationTests.java
@@ -0,0 +1,13 @@
+package bm.app.foodfindermap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodFinderMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
